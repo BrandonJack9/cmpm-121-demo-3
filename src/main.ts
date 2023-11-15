90cc984adfe410c5194dd5cd296011cb81efcb57
BrandonJack9 <bjack@ucsc.edu>
2023-11-15 01:18:17 -0800

    part a finished

--- src/main.ts
+++ src/main.ts
@@ -4,75 +4,114 @@ import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
-
 const MERRILL_CLASSROOM = leaflet.latLng({
-    lat: 36.9995,
-    lng: - 122.0533
+  lat: 36.9995,
+  lng: -122.0533,
 });
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const PIT_SPAWN_PROBABILITY = 0.1;
+const MAX_ZOOM = 19;
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-    center: MERRILL_CLASSROOM,
-    zoom: GAMEPLAY_ZOOM_LEVEL,
-    minZoom: GAMEPLAY_ZOOM_LEVEL,
-    maxZoom: GAMEPLAY_ZOOM_LEVEL,
-    zoomControl: false,
-    scrollWheelZoom: false
+  center: MERRILL_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: 0,
+  zoomControl: true,
+  scrollWheelZoom: true,
 });
 
-leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
-}).addTo(map);
+leaflet
+  .tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+      minZoom: 0,
+      maxZoom: MAX_ZOOM,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }
+  )
+  .addTo(map);
 
 const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
+
+const sensorButton = document.querySelector("#sensor")!;
+sensorButton.addEventListener("click", () => {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      console.log("position...", position);
+      playerMarker.setLatLng(
+        leaflet.latLng(position.coords.latitude, position.coords.longitude)
+      );
+      map.setView(playerMarker.getLatLng());
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+});
 
 let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
-    const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
-    ]);
+  const bounds = leaflet.latLngBounds([
+    [
+      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+    ],
+    [
+      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
+      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
+    ],
+  ]);
 
-    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+  const pit = leaflet.rectangle(bounds) as leaflet.Layer;
 
-
-
-    pit.bindPopup(() => {
-        let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-        const container = document.createElement("div");
-        container.innerHTML = `
+  pit.bindPopup(() => {
+    let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    const container = document.createElement("div");
+    container.innerHTML = `
                 <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">poke</button>`;
-        const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-        poke.addEventListener("click", () => {
-            value--;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
-            points++;
-            statusPanel.innerHTML = `${points} points accumulated`;
-        });
-        return container;
-    });
-    pit.addTo(map);
+                <button id="collect">collect</button>
+                <button id="deposit">deposit</button>`;
+    const collect = container.querySelector<HTMLButtonElement>("#collect")!;
+    const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
+
+    function updateCoins(isCollection: boolean) {
+      if (isCollection && value > 0) {
+        value--;
+        points++;
+      }
+      if (!isCollection && points > 0) {
+        value++;
+        points--;
+      }
+      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+        value.toString();
+
+      statusPanel.innerHTML = `${points} points accumulated`;
+    }
+
+    collect.addEventListener("click", () => updateCoins(true));
+    deposit.addEventListener("click", () => updateCoins(false));
+    return container;
+  });
+  pit.addTo(map);
 }
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-        if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-            makePit(i, j);
-        }
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+      makePit(i, j);
     }
+  }
 }
